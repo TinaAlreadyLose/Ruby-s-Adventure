b16cf5befe67a2df2ed2fcba5ec242d49c63ae1f
jiahao <jia.hao@fujisoft-china.com>
2020-12-04 15:45:32 +0800

    虽然有许多bug 但是也得提交啊 2020-12-4

--- Assets/Script/EnemyController.cs
+++ Assets/Script/EnemyController.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
-    public float speed = 1.0f;
+    public float speed;
     public bool vertical;
     public float changeTime = 3.0f;
+    public ParticleSystem smokeEffect;
 
     Rigidbody2D rigidbody2D;
     float timer;
     int direction = 1;
+    bool broken = true;
+
     Animator animator;
 
     // Start is called before the first frame update
@@ -19,10 +22,17 @@ public class EnemyController : MonoBehaviour
         rigidbody2D = GetComponent<Rigidbody2D>();
         timer = changeTime;
         animator = GetComponent<Animator>();
+        smokeEffect.Play();
     }
 
     void Update()
     {
+        //remember ! inverse the test, so if broken is true !broken will be false and return won’t be executed.
+        if (!broken)
+        {
+            return;
+        }
+
         timer -= Time.deltaTime;
 
         if (timer < 0)
@@ -34,19 +44,23 @@ public class EnemyController : MonoBehaviour
 
     void FixedUpdate()
     {
+        //remember ! inverse the test, so if broken is true !broken will be false and return won’t be executed.
+        if (!broken)
+        {
+            return;
+        }
+
         Vector2 position = rigidbody2D.position;
 
         if (vertical)
         {
             position.y = position.y + Time.deltaTime * speed * direction;
-            ;
             animator.SetFloat("MoveX", 0);
             animator.SetFloat("MoveY", direction);
         }
         else
         {
             position.x = position.x + Time.deltaTime * speed * direction;
-            ;
             animator.SetFloat("MoveX", direction);
             animator.SetFloat("MoveY", 0);
         }
@@ -62,5 +76,16 @@ public class EnemyController : MonoBehaviour
         {
             player.ChangeHealth(-1);
         }
+    }
+
+    //Public because we want to call it from elsewhere like the projectile script
+    public void Fix()
+    {
+        broken = false;
+        rigidbody2D.simulated = false;
+        // Destroy(gameObject);
+        //optional if you added the fixed animation
+        smokeEffect.Stop();
+        animator.SetTrigger("Fixed");
     }
 }
--- Assets/Script/RubyController.cs
+++ Assets/Script/RubyController.cs
@@ -5,32 +5,32 @@ using UnityEngine;
 public class RubyController : MonoBehaviour
 {
     public float speed = 3.0f;
+
     public int maxHealth = 5;
-    public float timeInvincible = 2.0f;
-    public  int health {get { return currentHealth; }}
+
+    public GameObject projectilePrefab;
+
+    public int health { get { return currentHealth; } }
     int currentHealth;
 
+    public float timeInvincible = 2.0f;
     bool isInvincible;
     float invincibleTimer;
 
-    private Rigidbody2D rigidbody2d;
+    Rigidbody2D rigidbody2d;
     float horizontal;
-
     float vertical;
 
-     Animator animator;
-
-    Vector2 lookDirectionVector2 = new Vector2(1, 0);
+    Animator animator;
+    Vector2 lookDirection = new Vector2(1, 0);
 
     // Start is called before the first frame update
     void Start()
     {
-        // QualitySettings.vSyncCount = 0;
-        // Application.targetFrameRate = 10;
         rigidbody2d = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
-        currentHealth = maxHealth; //获取默认生命值
-        currentHealth = maxHealth; //获取默认生命值
+
+        currentHealth = maxHealth;
     }
 
     // Update is called once per frame
@@ -38,19 +38,18 @@ public class RubyController : MonoBehaviour
     {
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
+
         Vector2 move = new Vector2(horizontal, vertical);
-        //检查x，y的值是否是零，Mathf.Approximately的精度更高
+
         if (!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
         {
-            lookDirectionVector2.Set(move.x, move.y);
-            lookDirectionVector2.Normalize();
-
-
+            lookDirection.Set(move.x, move.y);
+            lookDirection.Normalize();
         }
-        animator.SetFloat("Look X", lookDirectionVector2.x);
-        animator.SetFloat("Look Y", lookDirectionVector2.y);
+
+        animator.SetFloat("Look X", lookDirection.x);
+        animator.SetFloat("Look Y", lookDirection.y);
         animator.SetFloat("Speed", move.magnitude);
-    
 
         if (isInvincible)
         {
@@ -59,16 +58,19 @@ public class RubyController : MonoBehaviour
                 isInvincible = false;
         }
 
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            Launch();
+        }
     }
 
     void FixedUpdate()
     {
         Vector2 position = rigidbody2d.position;
-        // Debug.Log(horizontal);
-        position.x += (3f * horizontal * Time.deltaTime);
-        position.y += (3f * vertical * Time.deltaTime);
-        rigidbody2d.MovePosition(position);
+        position.x = position.x + speed * horizontal * Time.deltaTime;
+        position.y = position.y + speed * vertical * Time.deltaTime;
 
+        rigidbody2d.MovePosition(position);
     }
 
     public void ChangeHealth(int amount)
@@ -83,6 +85,17 @@ public class RubyController : MonoBehaviour
         }
 
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
-        Debug.Log(currentHealth + "/" + maxHealth);
+
+        UIHealthBar.instance.SetValue(currentHealth / (float)maxHealth);
+    }
+
+    void Launch()
+    {
+        GameObject projectileObject = Instantiate(projectilePrefab, rigidbody2d.position + Vector2.up * 0.5f, Quaternion.identity);
+
+        Projectile projectile = projectileObject.GetComponent<Projectile>();
+        projectile.Launch(lookDirection, 300);
+
+        animator.SetTrigger("Launch");
     }
 }
